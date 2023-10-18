3246dcc3af9d3e8c1552570eb9ab7f9785d72be5
takafumiTech <t.k.basket.f@gmail.com>
2023-10-18 22:05:52 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,17 @@ class User < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :consumption_type
        
+  with_options presence: true do
+
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+      validates :center_name
+    end
+
+    with_options format: { with: /\A[0-9]+\z/, messages: 'must be a number' } do
+      validates :center_code
+      validates :yearly_delivery
+    end
+  end
+
   validates :consumption_type_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
