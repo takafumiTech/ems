01ff6687e56dec9167a93e32d531ffc15f3cc1bd
takafumiTech <t.k.basket.f@gmail.com>
2023-10-21 18:24:19 +0900

    バリデーションの設定

--- app/models/daily_count.rb
+++ app/models/daily_count.rb
@@ -2,7 +2,7 @@ class DailyCount < ApplicationRecord
   belongs_to :user
 
   with_options presence: true do
-    validates :date
+    validates :date, uniqueness: true
 
     with_options numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9_999_999 },
     format: { with: /\A[0-9]+\z/ } do
