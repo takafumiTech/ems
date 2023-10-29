85bdce42ecbb7d0a11b10697dfff2d7711eeea85
takafumiTech <t.k.basket.f@gmail.com>
2023-10-29 23:03:19 +0900

    managementモデルの単体テスト

--- spec/models/management_spec.rb
+++ spec/models/management_spec.rb
@@ -12,7 +12,71 @@ RSpec.describe Management, type: :model do
       end
     end
     context '登録できない場合' do
-      
+      it "item_nameが空では登録できない" do
+        @management.item_name = ''
+        @management.valid?
+        expect(@management.errors.full_messages).to include("Item name can't be blank")
+      end
+      it "unitが空では登録できない" do
+        @management.unit = ''
+        @management.valid?
+        expect(@management.errors.full_messages).to include("Unit can't be blank")
+      end
+      it "unitが半角数字でなけれは登録できない" do
+        @management.unit = '二十六'
+        @management.valid?
+        expect(@management.errors.full_messages).to include("Unit is not a number")
+      end
+      it "lead_timeが空では登録できない" do
+        @management.lead_time = ''
+        @management.valid?
+        expect(@management.errors.full_messages).to include("Lead time can't be blank")
+      end
+      it "lead_timeが半角数字でなけれは登録できない" do
+        @management.lead_time = '二十六'
+        @management.valid?
+        expect(@management.errors.full_messages).to include("Lead time is not a number")
+      end
+      it "current_amountが空では登録できない" do
+        @management.current_amount = ''
+        @management.valid?
+        expect(@management.errors.full_messages).to include("Current amount can't be blank")
+      end
+      it "current_amountが半角数字でなけれは登録できない" do
+        @management.current_amount = '二十六'
+        @management.valid?
+        expect(@management.errors.full_messages).to include("Current amount is not a number")
+      end
+      it "spareが空では登録できない" do
+        @management.spare = ''
+        @management.valid?
+        expect(@management.errors.full_messages).to include("Spare can't be blank")
+      end
+      it "spareが半角数字でなけれは登録できない" do
+        @management.spare = '二十六'
+        @management.valid?
+        expect(@management.errors.full_messages).to include("Spare is not a number")
+      end
+      it "cunsumption_by_deliveryが空では登録できない" do
+        @management.consumption_by_delivery = ''
+        @management.valid?
+        expect(@management.errors.full_messages).to include("Consumption by delivery can't be blank")
+      end
+      it "cunsumption_by_repairが空では登録できない" do
+        @management.consumption_by_repair = ''
+        @management.valid?
+        expect(@management.errors.full_messages).to include("Consumption by repair can't be blank")
+      end
+      it "cunsumption_by_exchangeが空では登録できない" do
+        @management.consumption_by_exchange = ''
+        @management.valid?
+        expect(@management.errors.full_messages).to include("Consumption by exchange can't be blank")
+      end
+      it "userが紐ついていなければ登録できない" do
+        @management.user = nil
+        @management.valid?
+        expect(@management.errors.full_messages).to include("User must exist")
+      end
     end
   end
 end
